76af1ae6d4829190d363df1681a3e88b8cd26e46
Ahmed Elfarsisy <ahmedelfarsisy96@gmail.com>
2021-03-27 10:34:16 +0200

    create Newsly project

---
+++ app/src/main/java/com/iti/elfarsisy/mad41/newsly/util/MyApplication.kt
@@ -0,0 +1,31 @@
+package com.iti.elfarsisy.mad41.myapplication.util
+
+import android.app.Application
+import android.content.Context
+import com.iti.elfarsisy.mad41.newsly.BuildConfig
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+/*
+* Class MyApplication
+* config Timber
+* Get Application context
+* */
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = applicationContext
+        //plant timber
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+        Timber.i("Hello Application Class#########")
+    }
+
+
+    // contains function to return context
+    companion object {
+        private lateinit var INSTANCE: Context
+        fun getContext() = INSTANCE
+    }
+
+
+}
